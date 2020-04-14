7a46b90d054edfd5f9dabd264be5a5db222fcb15
luisalazago <luisalazago@gmail.com>
2020-04-13 20:51:53 -0500

    nuevo

---
+++ netflix/src/main/scala/Netflix/Netflix.scala
@@ -0,0 +1,12 @@
+package Netflix
+
+import scala.collection.mutable.ListBuffer
+
+class Netflix {
+    /*Atributos*/
+    private var peliculas = new ListBuffer[Pelicula]()
+    private var usuarios = new ListBuffer[Usuario]()
+    private var registros = new ListBuffer[Registro]()
+    private var planes = new ListBuffer[Plan]()
+    
+}
