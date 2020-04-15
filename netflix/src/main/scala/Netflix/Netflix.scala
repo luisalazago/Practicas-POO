8989cdd7429e1dc18eebc7ac954308d3dd3d5a3b
luis <luisalazago@gmail.com>
2020-04-15 18:17:46 -0500

    nuevo 1.0

--- netflix/src/main/scala/Netflix/Netflix.scala
+++ netflix/src/main/scala/Netflix/Netflix.scala
@@ -1,6 +1,9 @@
 package Netflix
 
-class Netflix {
+import Usuario.Usuario
+import Usuario.UsuarioNormal
+
+class Netflix extends Usuario with UsuarioNormal {
     /*Atributos*/
     var _peliculas : List[Pelicula] = List()
     var _usuarios : List[Usuario] = List()
@@ -11,12 +14,33 @@ class Netflix {
 
     /*Métodos*/
     def registroUsuario(cuenta : String, contra : String) : Unit = {
-        println("0")
+        var usuarioTemp : Usuario = _
+        usuarioTemp.nombre = cuenta
+        usuarioTemp.contraseña = contra
+        usuarios += usuarioTemp
     }
-    def logeoUsuario(cuenta : String, contra : String) : Unit = {
-        println("0")
+    def logeoUsuario(cuenta : String, contra : String) : Boolean = {
+        var pos : Double = _ 
+        var pos2 : Double = _ 
+        for(n <- _usuarios) {
+            if(n.nombre == cuenta) {
+                pos2 = pos
+            }
+            pos = pos + 1
+        }
+        if(_usuarios(pos2)) {
+            if(contra == _usuarios(pos2).contraseña) {
+                return true
+            }
+        }
+        return false
     }
-    def buscarPelicula(nombre : String) : Unit = {
-        println("0")
+    def buscarPelicula(nombre : String) : Boolean = {
+        for(n <- _peliculas) {
+            if(nombre == n) {
+                return true
+            }
+        }
+        return false
     }
 }
