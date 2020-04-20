2e52a5d72403a27c561ca37aaf9bfbeb0e7275e8
Geiler20182 <44622578+Geiler20182@users.noreply.github.com>
2020-04-19 23:03:47 -0500

    Dependencias

--- Netflix/src/main/scala/Test/Test.scala
+++ Netflix/src/main/scala/Test/Test.scala
@@ -12,12 +12,7 @@ object Test {
 
     def main(args: Array[String]) : Unit = {
 
-        /* Test */ 
-        var nuevo_plan : Plan = new Plan
-        var nuevo_usuario : UsuarioAdmin = new UsuarioAdmin("Geiler@org", "%$#", "Geiler")
-        var nueva_factura : Factura = new Factura
-        println("=======================")
-        println("=======================")
+        println("NETFLIX!")
     }   
 
 }
